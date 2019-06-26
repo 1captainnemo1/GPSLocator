b90b59f81534654e9b9ce1bf11e45a7018d55ffc
1captainnemo1 <37461247+1captainnemo1@users.noreply.github.com>
2019-06-27 02:56:18 +0530

    Update locator.js

--- locator.js
+++ locator.js
@@ -1,4 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type='text/javascript'>
 function httpGet(theUrl)
 {
